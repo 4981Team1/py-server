abc9d695fca91f5d47279dc05385382668020f0d
Mikhaela Layon <mapl.layon@gmail.com>
2021-02-08 07:45:39 -0800

    rewrote routes to work with pymongo
    added the following:
    - results route which returns JSON of vote count
    - vote route which allows user to vote

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,31 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+# from flask_mongoengine import MongoEngine
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
+app = Flask(__name__, instance_relative_config=False)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/flaskdb")
+db = mongo.db
 
+# collections needed can go here
+voter = db.voter
+ballot = db.ballot
+election = db.election
 
 def create_app():
     """Construct the core application."""
-    app = Flask(__name__, instance_relative_config=False)
+    # app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
+    
 
-    db.init_app(app)
+    # db.init_app(app)
+    # app.config["MONGO_URI"] = "mongodb://localhost:27017/flaskdb"
+    # mongo = PyMongo(app)
+    # db_operations = mongo.db.voter
 
     with app.app_context():
         from . import routes  # Import routes
-        db.create_all()  # Create sql tables for our data models
+        # db.create_all()  # Create sql tables for our data models
 
         return app
--- app/routes.py
+++ app/routes.py
@@ -1,70 +1,94 @@
-from flask import make_response, redirect, render_template, request, url_for
+from flask import make_response, redirect, render_template, request, url_for, jsonify
 from flask import current_app as app
-from .models import db, Users
-
+# from .models import db, Users # not currently used
+from . import voter, ballot, election
 # # Turns debugging on (auto reload)
 # app.run(debug=True)
 
-@app.route('/', methods=['GET'])
-def user_records():
-    """Create a user via query string parameters."""
-    username = request.args.get('user')
+# Grabs all voters
+# http://localhost:5000/
+@app.route('/')
+def voter_records():
+    # Creates a new election if there's no exising election document in DB
+    filt = {'details' : 'test'}
+    update = { "$setOnInsert": { 'choices': ['a', 'b', 'c'], 'details': 'test' }}
+    election.update_one(filt, update, upsert=True)
+
+    # Grabs all users
+    voters = voter.find()
+    output = [{'name' : voter['name'], 'email' : voter['email']} for voter in voters]
+    print(output)
+    return jsonify(output)
+
+# Updates one voter's email based on name, it would 
+# insert a new voter if it doesn't already exist.
+# http://localhost:5000/update?name=[NAME]&email=[EMAIL]
+# e.g. http://localhost:5000/update?name=grover&email=another@email.com
+@app.route('/update')
+def update():
+    name = request.args.get('name')
     email = request.args.get('email')
-    if username and email:
-        existing_user = Users.query.filter(
-            Users.username == username or Users.email == email
-        ).first()
-        if existing_user:
-            return render_template(
-                'update_user.html',
-                title = "Update User",
-                username = username,
-                email = email)
-            # return make_response(
-            #     # Ask if you would like to update or delete
-            #     f'{username} ({email}) already created!'
-            # )
-        new_user = Users(
-            username=username,
-            email=email
-        )  # Create an instance of the User class
-        db.session.add(new_user)  # Adds new User record to database
-        db.session.commit()  # Commits all changes
-        redirect(url_for('user_records'))
-    return render_template(
-        'users.html',
-        users=Users.query.all(),
-        title="Show Users"
-    )
+    updated_voter = {"$set": {'email' : email}}
+    filt = {'name' : name}
+    voter.update_one(filt, updated_voter, upsert=True)
+    result = {'result' : 'Updated successfully'}
+    return result;
+    
+# Deletes one voter based on name
+# http://localhost:5000/delete?name=[NAME]
+# e.g. http://localhost:5000/delete?name=grover
+@app.route('/delete')
+def delete():
+    name = request.args.get('name')
+    filt = {'name' : name}
+    voter.delete_one(filt)
+    result = {'result' : 'Deleted successfully'}
+    return result;
 
-@app.route('/update_user', methods=['POST'])
-def update_user():
-    id = Users.id
-    username = request.form['username']
-    email = request.form['email']
-    if username and email:
-        existing_user = Users.query.filter(
-            Users.id == id
-        ).first()
-        existing_user.username = username
-        existing_user.email = email
-        db.session.commit() # Updates username and email
-    return render_template(
-        'users.html',
-        users=Users.query.all(),
-        title="Show Users"
-    )
+# Records the MOST RECENT vote made by a voter into their ballot.
+# If they haven't voted before, a new Ballot document will be created.
+# Otherwise, the ballot will be updated only with their most recent choice.
+# 
+# http://localhost:5000/vote?name=[NAME]&choice=[CHOICE]
+# e.g. http://localhost:5000/vote?name=grover&choice=a
+@app.route('/vote')
+def vote():
+    
+    # Get url params
+    name = request.args.get('name')
+    choice = request.args.get('choice')
+    
+    # Get objectID of test election
+    elec_id = str(election.find_one({'details' : 'test'})['_id']) #need to convert objectid to string
+    # print(elec_id)
 
-@app.route('/delete_user', methods=['POST'])
-def delete_user():
-    id = Users.id
-    user = Users.query.filter(
-        Users.id == id
-    ).first()
-    db.session.delete(user)
-    db.session.commit() # Deletes user
-    return render_template(
-        'users.html',
-        users=Users.query.all(),
-        title="Show Users"
-    )
+    # Get voterID of the voter specified by params
+    voter_id = str(voter.find_one({'name' : name})['_id'])
+
+    # Upsert new ballot document
+    filt = {'election_id' : elec_id, 'voter_id' : voter_id}
+    new_ballot = {"$set": {'choice' : choice, 'election_id' : elec_id, 'voter_id' : voter_id }}
+    # ballot.insert_one(new_ballot)
+    ballot.update_one(filt, new_ballot, upsert=True)
+    result = {'result' : 'Voted successfully'}
+    return result
+
+# Outputs the vote count as a JSON object
+# http://localhost:5000/results
+@app.route('/results')
+def results():
+
+    # Get objectID of test election
+    elec = election.find_one({'details' : 'test'})
+    elec_id = str(elec['_id']) #need to convert objectid to string
+    elec_choices = elec['choices']
+
+    ballots = ballot.find({'election_id' : elec_id})
+    votes = [b['choice'] for b in ballots] # getting all the votes from the ballots
+    # print(votes)
+
+    # Contains vote count
+    output = [{c : votes.count(c)} for c in elec_choices]
+        
+    print(output)
+    return jsonify(output)
