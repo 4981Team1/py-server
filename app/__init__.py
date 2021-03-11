3b1585d9dcf48fa465607a24d281ba4c5a46cc35
Mikhaela Layon <mapl.layon@gmail.com>
2021-03-11 01:40:59 -0800

    cleaned up code

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
-# from flask_mongoengine import MongoEngine
 
 # db = SQLAlchemy()
 app = Flask(__name__, instance_relative_config=False)
-# uri = "mongodb+srv://admin:goodteam@cluster0.x0m4l.mongodb.net/flaskdb?retryWrites=true&w=majority"
+uri = "mongodb+srv://admin:goodteam@cluster0.x0m4l.mongodb.net/flaskdb?retryWrites=true&w=majority"
 # uri = "mongodb://localhost:27017/flaskdb" # uri for local db
-uri = "mongodb://localhost:27017/py_server" # uri for brian local db
+# uri = "mongodb://localhost:27017/py_server" # uri for brian local db
 mongo = PyMongo(app, uri)
-# mongo = PyMongo(app, uri) # for connecting to hosted db
 db = mongo.db
 
 # collections needed can go here
@@ -24,7 +21,7 @@ def create_app():
     
 
     with app.app_context():
-        # from . import routes  # Import routes
+        from . import routes  # Import routes
         from . import voters
         from . import elections
         from . import ballots
