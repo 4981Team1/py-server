741e2ffee8508e722d7194748da7362f96ad561a
agoryelov <andrey.goryelov@gmail.com>
2021-03-23 18:49:17 -0700

    Making dummy auth routes available

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,14 @@
-from flask import make_response, redirect, render_template, request, url_for, jsonify
+from flask import json, make_response, redirect, render_template, request, url_for, jsonify
 from flask import current_app as app
 from . import voter, ballot, election
 from bson.objectid import ObjectId
 
 @app.route('/register', methods = ['POST'])
 def register():
-    return "Register route"
+    output = { 'success': True }
+    return jsonify(output), 200
 
 @app.route('/login', methods = ['POST'])
 def login():
-    return "Login route"
+    output = { 'token': 'AUTH-JWT-TOKEN' }
+    return jsonify(output), 200
