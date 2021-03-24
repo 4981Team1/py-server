1b1dff9481b26ff9ca344c990dd3351f5e8f9bc0
agoryelov <andrey.goryelov@gmail.com>
2021-03-23 21:19:46 -0700

    Added jwt token generation

--- app/auth.py
+++ app/auth.py
@@ -3,7 +3,9 @@ from flask import current_app as app
 from . import voter
 from bson.objectid import ObjectId
 from . import voter
+import jwt
 import bcrypt
+import datetime as dt
 
 @app.route('/register', methods = ['POST'])
 def register():
@@ -22,6 +24,7 @@ def register():
     raw_password = body['password'].encode()
     hashed = bcrypt.hashpw(raw_password, bcrypt.gensalt())
     voter.insert({'email': body['email'], 'password': hashed})
+    
     
     output = { 'success': True, 'error': '' }
     return jsonify(output), 200
@@ -44,7 +47,30 @@ def login():
     if not bcrypt.checkpw(raw_password, v['password']):
         output['error'] = f"No user for email {body['email']}"
         return jsonify(output), 400
+    
+    # {"exp": 1371720939}
+    expires_on = dt.datetime.utcnow() + dt.timedelta(seconds=60)
+    encoded_jwt = jwt.encode({ "exp": expires_on }, "secret-phrase-abc", algorithm="HS256")
 
     # jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
-    output = { 'success': True, 'error': '', 'token': 'AUTH-JWT-TOKEN' }
+    output = { 'success': True, 'error': '', 'token': encoded_jwt }
+    return jsonify(output), 200
+
+@app.route('/jwt-protected', methods = ['GET'])
+def jwt_protected():
+    output = { 'success': False, 'error': '' }
+
+    if 'voter-token' not in request.headers:
+        output['error'] = "Missing token: voter-token"
+        return jsonify(output), 201
+    
+    encoded_jwt = request.headers['voter-token']
+
+    try:
+        jwt.decode(encoded_jwt, "secret-phrase-abc", algorithms=["HS256"])
+    except jwt.InvalidTokenError:
+        output['error'] = "Invalid token: voter-token"
+        return jsonify(output), 201
+    
+    output = { 'success': True, 'error': '' }
     return jsonify(output), 200
