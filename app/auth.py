ab8251226e693b01c873bd78bb7ab589d0ee2870
agoryelov <andrey.goryelov@gmail.com>
2021-03-23 18:38:58 -0700

    heroku

---
+++ app/auth.py
@@ -0,0 +1,12 @@
+from flask import make_response, redirect, render_template, request, url_for, jsonify
+from flask import current_app as app
+from . import voter, ballot, election
+from bson.objectid import ObjectId
+
+@app.route('/register', methods = ['POST'])
+def register():
+    pass
+
+@app.route('/login', methods = ['POST'])
+def login():
+    pass
